edb852d31a50a904229ba432908ff2ef3fc14bc6
SIBOK <maelstrom@mail.ru>
2020-05-07 15:00:55 +0400

    Home Work №8 v.1

--- lesson_8/exercise_4.py
+++ lesson_8/exercise_4.py
@@ -7,31 +7,43 @@ class Storage:
 
     __storage_capacity = 100
 
-    def __init__(self, total_amount):
-        self.total_amount = total_amount
+    def __init__(self):
+        self.total_amount = 0
+        self.storage = {}
+
+    def add(self, data, model):
+        self.storage[model] = data
+
+    def scan(self, model):
+        return self.storage[model]
 
 class Equipment:
 
-    def __init__(self, tool_type, model, coast, amount):
-        self.tool_type = tool_type
+    def __init__(self, model, coast, amount):
         self.model = model
         self.coast = coast
         self.amount = amount
 
 class Printer(Equipment):
 
-    def __init__(self, tool_type, model, coast, amount, print_speed):
-        super().__init__(tool_type, model, coast, amount)
+    def __init__(self, model, coast, amount, print_speed):
+        super().__init__(model, coast, amount)
         self.print_speed = print_speed
 
 class Copier(Equipment):
 
-    def __init__(self, tool_type, model, coast, amount, type_install):
-        super().__init__(tool_type, model, coast, amount)
+    def __init__(self, model, coast, amount, type_install):
+        super().__init__(model, coast, amount)
         self.type_install = type_install
 
 class Scanner(Equipment):
 
-    def __init__(self, tool_type, model, coast, amount, resolution):
-        super().__init__(tool_type, model, coast, amount)
+    def __init__(self, model, coast, amount, resolution):
+        super().__init__(model, coast, amount)
         self.resolution = resolution
+
+
+p = Printer(1, 2, 3, 4)
+sklad = Storage()
+sklad.add(p, 1)
+print(sklad.scan(1))
