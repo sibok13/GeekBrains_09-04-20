060453112aaba3deeb927b1376a1fd8c6db61b59
SIBOK <maelstrom@mail.ru>
2020-05-10 16:06:59 +0400

    Home Work №8 v.4

--- lesson_8/exercise_7.py
+++ lesson_8/exercise_7.py
@@ -3,7 +3,31 @@
 # и выполнив сложение и умножение созданных экземпляров. Проверьте корректность полученного результата.
 
 
+class Complex:
+    def __init__(self, d, m):
+        self.d = d
+        self.m = m
+
+    def __str__(self):
+        tmp = '+' if self.m >= 0 else ''
+        return f'{self.d}{tmp}{self.m}j'
+
+    __repr__ = __str__
 
 
+class Calc:
 
-# думаю, решаю, постараюсь в понедельник доделать, что-то сложно очень
+    def summ(self, c1, c2):
+        d = c1.d + c2.d
+        m = c1.m + c2.m
+        print(Complex(d, m))
+
+    def mul(self, c1, c2):
+        d = c1.d * c2.d - c1.m * c2.m
+        m = c1.m * c2.d + c1.d * c2.m
+        print(Complex(d, m))
+
+
+calc = Calc()
+calc.summ(Complex(2, 4), Complex(1, 2))
+calc.mul(Complex(2, 4), Complex(1, 2))
